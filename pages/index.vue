e21a2d567869ead5f54df7006c64eba9c31cec65
Chazz-John <chazz0504@qq.com>
2021-12-02 01:01:36 +0800

    路由,动态路由,嵌套路由

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <h3>index page</h3>
+    <!--普通路由-->
     <NuxtLink to="/detail">Detail Page</NuxtLink>
+    <br/>
+    <!--动态路由-->
+    <NuxtLink to="/user-admin/1">user page</NuxtLink>
+    <br/>
+    <!--嵌套路由-->
+    <NuxtLink to="/parent/child">parent page</NuxtLink>
   </div>
 </template>
+<script setup>
+</script>
---
+++ pages/parent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>parent</h1>
+    <NuxtChild></NuxtChild>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
