edaafe79fffda47aa75847fbc6a91a57233994ac
Chazz-John <chazz0504@qq.com>
2021-12-05 23:11:04 +0800

    跨组件共享状态

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,11 @@
       <input type="checkbox" v-model="todo.completed">
       <strong>{{todo.title}}</strong>
     </div>
+    <br/>
+
+    <button @click="counter++">+</button>
+    {{counter}}
+    <button @click="counter--">-</button>
 
     <!--普通路由-->
     <NuxtLink to="/detail">Detail Page</NuxtLink>
@@ -28,12 +33,15 @@
 </template>
 <script setup lang="ts">
   import { ref } from 'vue'
+  import { useCounter } from '~~/composables/useCounter'
   const show = ref(false)
   // const {data:todos} = await useAsyncData('todos',()=>$fetch('/api/todo')) 
   const {data:todos}:any = await useFetch('/api/todo',{
     transform(input:{data:[]}){
       return input.data
     }
-  }) 
-
+  })
+  console.log(todos.value)
+  //声明ssr友好状态
+  let counter = useCounter()
 </script>
